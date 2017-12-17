b5b136ec5ff50a962d13149ea18ddbc06fb072ff
lukaszsadlocha <lukaszsadlocha@gmail.com>
2017-12-18 00:07:39 +0100

    Add 3-column component and component to render cars

---
+++ SitecoreMvcPlayground/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecoreMvcPlayground.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View(DateTime.Now);
+        }
+    }
+}
