b94c7f70f366859941237b31a08b4033b88ccd73
lukaszsadlocha <lukaszsadlocha@gmail.com>
2017-12-21 00:39:31 +0100

    Add FeaturedCar which is ViewRenfering with custom model

---
+++ SitecoreMvcPlayground/Helpers/SitecoreFieldHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Mvc.Helpers;
+using Sitecore.Data.Items;
+
+namespace SitecoreMvcPlayground.Helpers
+{
+    public static class SitecoreFieldHelper
+    {
+        public static HtmlString ImageField(this SitecoreHelper helper, string fieldName, Item item, int mh, int mw, bool disableWebEditing = false)
+        {
+            return helper.Field(fieldName, item, new { mh, mw, disableWebEditing });
+
+        }
+    }
+}
