c5e646f72a452d1313f8f8e2bc61065ed23d4090
lukaszsadlocha <lukaszsadlocha@gmail.com>
2017-12-30 17:55:26 +0100

    Add Driver repository

---
+++ SitecoreMvcPlayground/Repositioris/DriverRepository.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Mvc.Presentation;
+using Sitecore.Web.UI.WebControls;
+using SitecoreMvcPlayground.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreMvcPlayground.Repositioris
+{
+    public class DriverRepository
+    {
+        public Driver GetDriver()
+        {
+            var driver = new Driver();
+            var rendering = RenderingContext.Current.Rendering;
+
+            var datasource = rendering.Item;
+
+            driver.Name = new HtmlString(FieldRenderer.Render(datasource, "Name"));
+            driver.Text = new HtmlString(FieldRenderer.Render(datasource, "Text"));
+
+            return driver;
+        }
+    }
+}
